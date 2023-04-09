e4fa63b4c7868a5c056e47bb7b4911f8ab767f55
Max3281 <113448092+Max3281@users.noreply.github.com>
2023-04-09 16:31:19 +0300

    edit 1

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -3,7 +3,21 @@ import {
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
+import propTypes from 'prop-types';
+import { BiBeer } from 'react-icons/bi';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+export const StatisticItem = ({ title, total, icon: Icon }) => {
+  return (
+    <StatisticBox>
+      <Icon color="blue" size="30px" />
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
+};
+
+StatisticItem.propTypes = {
+  title: propTypes.string.isRequired,
+  total: propTypes.number.isRequired,
+  icon: propTypes.func.isRequired,
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,11 +3,43 @@ import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
+import propTypes from 'prop-types';
 
-export const Statistics = () => {
+const icons = {
+  1: FaRegThumbsUp,
+  2: MdPeople,
+  3: MdOutlineProductionQuantityLimits,
+  4: GiTreeDoor,
+};
+
+export const Statistics = ({ statTitle, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {statTitle && <StatisticTitle>{statTitle}</StatisticTitle>}
+
+      <StatisticsList>
+        {stats.map(({ id, title, total }) => {
+          return (
+            <StatisticItem
+              key={id}
+              title={title}
+              total={total}
+              icon={icons[id]}
+            />
+          );
+        })}
+      </StatisticsList>
     </>
   );
+};
+
+Statistics.propTypes = {
+  statTitle: propTypes.string,
+  stats: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.string.isRequired,
+      title: propTypes.string.isRequired,
+      total: propTypes.number.isRequired,
+    }),
+  ).isRequired,
 };
